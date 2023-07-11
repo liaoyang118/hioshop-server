904c3e1ea4bc1845383339457ded99d7f09245e2
廖杨 <liao.yang118@163.com>
2023-07-11 08:49:38 +0000

    add dockerfile

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
     database: 'hiolabsDB',
     prefix: 'hiolabs_',
     encoding: 'utf8mb4',
-    host: '127.0.0.1',
+    host: '150.158.51.160',
     port: '3306',
     user: 'root',
-    password: '123123123',
+    password: '123456',
     dateStrings: true
 };
