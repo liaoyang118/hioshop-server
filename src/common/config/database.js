bb8342a4df5b20e9b912edf5f395dd7331c60b20
廖杨 <liao.yang118@163.com>
2023-08-09 14:46:41 +0800

    Update database.js

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -5,9 +5,9 @@ module.exports = {
     database: 'hiolabsDB',
     prefix: 'hiolabs_',
     encoding: 'utf8mb4',
-    host: '150.158.51.160',
+    host: '',
     port: '3306',
-    user: 'nodeuser',
-    password: '123456',
+    user: '',
+    password: '',
     dateStrings: true
 };
